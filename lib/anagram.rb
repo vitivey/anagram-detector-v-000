1dc20ec402ca8647af4fd057d33a86a64f33c084
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-14 22:33:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+attr_accessor :word
+
+    def initialize(word)
+      @word=word
+    end
+
+    def match(array)
+      match=[]
+
+      array.each do |aword|
+        match << aword if @word.split("").sort == aword.split("").sort
+        end
+      match
+    end
+
+end
