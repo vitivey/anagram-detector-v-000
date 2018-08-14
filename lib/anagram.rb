da45540f9b2ca9c22e8e916ad69fd5e41de47b2f
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-14 22:27:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+    def initialize(word)
+      @word=word
+    end
+
+    def match(array)
+      match=[]
+
+      array.each do |aword|
+        match << aword if @word.split.sort == aword.split.sort
+        end
+
+      match        
+      end
+      #>return all matches or empty array
+    end
+
+end
